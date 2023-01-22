267fbd50fdde3bea3679cbb8b207172ee56580c1
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-01-22 22:36:32 +0100

    first commit

---
+++ src/components/counter.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <div className="card-header">
+                    <strong>
+                        {this.props.title} : {this.props.value}
+                    </strong>
+                </div>
+                <div className="card-body">
+                    <img width={100} src={this.props.image}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Counter;
